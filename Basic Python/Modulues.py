db07bde41878a5ae9371ca4870577a7f56daa0cc
Bohara <mr.dhanushbohara@gmail.com>
2026-04-14 06:51:59 +0545

    change commit

--- Basic Python/Modulues.py
+++ Basic Python/Modulues.py
@@ -55,4 +55,49 @@ print(os.getcwd(), ' || get current working directory.')
 print(os.listdir('.'), ' || list of files on thi dir')
 print(os.path.exists('test.py'), ' || is test.py file is exists print true or false. ')
 
-os.makedirs()
+print('_______________________________________Daily Planner ______________________________________')
+from datetime import datetime, date
+import random 
+motivational_quotes = ['The secret of getting ahead gettting stared.',
+                       "Belive you can and you're halfway there.",
+                       'Success is not final, failure is not fatal.',
+                       'The future blongs to those who belive in their deams.']
+
+task = []
+def show_header():
+    now= datetime.now()
+    print('___________________________________')
+    print('Daily Planner')
+    print(f"{now.strftime('%A, %B %d, %Y $H:%M')}")
+    print(f"Quote: {random.choise(motivational_quotes)}")
+    print()
+
+show_header()
+
+while True:
+    print(F"1. add 2.View tasks 3.mark down 4.Exit")
+    choice = input(">>>")
+
+    if choice == '1':
+        tasks = input("New task: ")
+        time = input("Due time (eg. 3:00 PM): ")
+        task.append({'task': task, 'time': time, 'done': False})
+        print('task added')
+    
+    elif choice == '2':
+        if not tasks:
+            print("No tasks yet!")
+        for i, t in enumerate(tasks, 1):
+            status = '✓' if t['done'] else '○'
+            print(f" {status} [{i}] {t['time']} - {t['task']}")
+    
+    elif choice == '3':
+        num = int(input("Task number to mark done: ")) - 1
+        if 0 <= num < len(tasks):
+            tasks[num]['done'] = True
+            print("Marked as done!")
+    
+    elif choice == '4':
+        completed = sum(1 for t in tasks if t['done'])
+        print(f"Session complete! {completed}/{len(tasks)} tasks done.")
+        break
